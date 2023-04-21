57b2983460adb56fdf928edb34b0f67e9378abbb
Marek Gajewski <marek.gajewski@logiblox.com>
2023-04-21 16:11:51 +0200

    feat: nbp handler works

--- api/services/app.py
+++ api/services/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, request
-from nbp_handler import get_exchange_rate, get_max_min_average, get_major_difference
+from nbp_handler import get_average_exchange_rate, get_max_min_average, get_major_difference
 #from instance.config import Config
 
 app = Flask(__name__)
 #app.config.from_object(Config)
 
 @app.route('/exchanges/<currency>/<date>')
-def get_exchange_rate_route(currency, date):
-    rate = get_exchange_rate(currency, date)
+def get_average_exchange_rate_route(currency, date):
+    rate = get_average_exchange_rate(currency, date)
     if rate is None:
         return jsonify(error='Data not available'), 404
     return jsonify(rate=rate)
--- api/services/nbp_handler.py
+++ api/services/nbp_handler.py
@@ -1,23 +1,25 @@
 import requests
 from datetime import datetime, timedelta
+import json
 
 NBP_BASE_URL = 'http://api.nbp.pl/api/exchangerates'
 
-def get_average_exchange_rate(date: str, currency: str) -> float:
+def get_average_exchange_rate(currency: str, date: str) -> float:
     """
     Given a date (formatted YYYY-MM-DD) and a currency code, returns the
     average exchange rate for that currency on the given date.
     """
     url = f'{NBP_BASE_URL}/rates/A/{currency}/{date}/'
     response = requests.get(url)
-    data = response.json()
+    content = response.content.decode('utf-8-sig')
+    data = json.loads(content)
     rates = data.get('rates', [])
     if len(rates) > 0:
         return rates[0].get('mid', 0)
     else:
         return 0
 
-def get_min_max_average(currency: str, n: int) -> dict:
+def get_max_min_average(currency: str, n: int) -> dict:
     """
     Given a currency code and the number of last quotations, returns a
     dictionary with the max and min average exchange rates for that currency
@@ -44,7 +46,7 @@ def get_min_max_average(currency: str, n: int) -> dict:
         avg_rate = total / len(rates)
         return {'max': max_rate, 'min': min_rate, 'average': avg_rate}
 
-def get_buy_sell_difference(currency: str, n: int) -> float:
+def get_major_difference(currency: str, n: int) -> float:
     """
     Given a currency code and the number of last quotations, returns the
     major difference between the buy and sell rates over the last n days.
