e3c4a97e93f2923954311a051f05ae5a276d8234
Marek Gajewski <marek.gajewski@logiblox.com>
2023-04-23 20:26:38 +0100

    comments

--- api/services/app.py
+++ api/services/app.py
@@ -1,33 +1,50 @@
 from flask import Flask, jsonify, request
 from nbp_handler import get_average_exchange_rate, get_max_min_average, get_major_difference
 from flask_cors import CORS
-
 from config import Config
 
+# Create a new Flask app instance
 app = Flask(__name__)
+
+# Enable Cross-Origin Resource Sharing (CORS) for the app
 CORS(app)
+
+# Load configuration from config.py file
 app.config.from_object(Config)
 
+# Define a route for getting the average exchange rate for a currency and date
 @app.route('/exchanges/<currency>/<date>')
 def get_average_exchange_rate_route(currency, date):
+    # Call the get_average_exchange_rate function from nbp_handler to get the rate
     rate = get_average_exchange_rate(currency, date)
+    # If the rate is None (i.e. data is not available), return a 404 error message
     if rate is None:
         return jsonify(error='Data not available'), 404
+    # Otherwise, return the rate in a JSON format
     return jsonify(rate=rate)
 
+# Define a route for getting the max, min, and average exchange rates for a currency over a specified number of days
 @app.route('/exchanges/<currency>/max-min/<int:n>')
 def get_max_min_average_route(currency, n):
+    # Call the get_max_min_average function from nbp_handler to get the result
     result = get_max_min_average(currency, n)
+    # If the result is None (i.e. data is not available), return a 404 error message
     if result is None:
         return jsonify(error='Data not available'), 404
+    # Otherwise, return the result in a JSON format
     return jsonify(result)
 
+# Define a route for getting the major differences in exchange rates for a currency over a specified number of days
 @app.route('/exchanges/<currency>/major-difference/<int:n>')
 def get_major_difference_route(currency, n):
+    # Call the get_major_difference function from nbp_handler to get the result
     result = get_major_difference(currency, n)
+    # If the result is None (i.e. data is not available), return a 404 error message
     if result is None:
         return jsonify(error='Data not available'), 404
+    # Otherwise, return the result in a JSON format
     return jsonify(result)
 
+# Start the app if this script is being run directly (i.e. not imported as a module)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- api/services/nbp_handler.py
+++ api/services/nbp_handler.py
@@ -10,14 +10,20 @@ def get_average_exchange_rate(currency: str, date: str) -> float:
     average exchange rate for that currency on the given date.
     """
     url = f'{NBP_BASE_URL}/rates/A/{currency}/{date}/'
+    # Construct the API URL to fetch the exchange rate for a given currency on a given date
     response = requests.get(url)
+    # Send a GET request to the API and get the response
     content = response.content.decode('utf-8-sig')
+    # Decode the response content
     data = json.loads(content)
+    # Parse the response content as JSON data
     rates = data.get('rates', [])
+    # Get the rates data from the response, or an empty list if there is no data
     if len(rates) > 0:
         return rates[0].get('mid', 0)
     else:
         return 0
+    # Return the average exchange rate, or 0 if there is no data
 
 def get_max_min_average(currency: str, n: int) -> dict:
     """
@@ -26,44 +32,68 @@ def get_max_min_average(currency: str, n: int) -> dict:
     over the last n days.
     """
     today = datetime.today().strftime('%Y-%m-%d')
+    # Get the current date in the format YYYY-MM-DD
     url = f'{NBP_BASE_URL}/rates/A/{currency}/last/{n}/'
+    # Construct the API URL to fetch the last n days of exchange rate data for a given currency
     response = requests.get(url)
+    # Send a GET request to the API and get the response
     data = response.json()
+    # Parse the response content as JSON data
     rates = data.get('rates', [])
+    # Get the rates data from the response, or an empty list if there is no data
     if len(rates) == 0:
         return {'max': 0, 'min': 0}
     else:
         total = 0
         min_rate = float('inf')
         max_rate = float('-inf')
+        # Initialize the total exchange rate, minimum exchange rate, and maximum exchange rate
         for rate in rates:
             mid = rate.get('mid', 0)
             total += mid
+            # Add the mid exchange rate to the total
             if mid < min_rate:
                 min_rate = mid
+            # Update the minimum exchange rate if the current mid exchange rate is lower
             if mid > max_rate:
                 max_rate = mid
+            # Update the maximum exchange rate if the current mid exchange rate is higher
         avg_rate = total / len(rates)
+        # Calculate the average exchange rate over the last n days
         return {'max': max_rate, 'min': min_rate, 'average': avg_rate}
+    # Return a dictionary containing the max, min, and average exchange rates
 
 def get_major_difference(currency: str, n: int) -> float:
     """
     Given a currency code and the number of last quotations, returns the
     major difference between the buy and sell rates over the last n days.
     """
+    # Get the current date
     today = datetime.today().strftime('%Y-%m-%d')
+    # Build the URL for the API call
     url = f'{NBP_BASE_URL}/rates/C/{currency}/last/{n}/'
+    # Send the request to the API and get the response
     response = requests.get(url)
+    # Extract the JSON data from the response
     data = response.json()
+    # Get the list of rates from the JSON data
     rates = data.get('rates', [])
     if len(rates) == 0:
+        # If there are no rates, return 0
         return 0
     else:
+        # Otherwise, initialize the max_diff variable to negative infinity
         max_diff = float('-inf')
+        # Iterate over each rate in the list
         for rate in rates:
+            # Get the bid and ask prices from the rate
             bid = rate.get('bid', 0)
             ask = rate.get('ask', 0)
+            # Calculate the difference between the ask and bid prices
             diff = ask - bid
+            # If the difference is greater than the current max_diff, update max_diff
             if diff > max_diff:
                 max_diff = diff
+        # Return the maximum difference between the buy and sell prices
         return max_diff
+
